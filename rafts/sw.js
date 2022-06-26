61e0ccc66ee6768fdb728e76d64ab514c22ddcf4
Ttmdacl <55438621+Ttmdacl@users.noreply.github.com>
2022-06-27 02:54:13 +0900

    괄호 수정

--- rafts/sw.js
+++ rafts/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220627.0242';
+const cacheName = 'chirpy-20220627.0253';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
